23734435a16fa39e11642998c5b40744d19de556
miriamcrystal <miriam@ruzuku.com>
2015-06-03 14:50:59 -0400

    adding template

---
+++ src/js/template.js
@@ -0,0 +1,15 @@
+_.templateSettings.variable = "m";
+
+var template = _.template(
+            $( "script.template" ).html()
+        );
+
+
+var templateData = {
+
+  name: 'miriamcrystal',
+  avatar_url: "",
+  public_repos: 34,
+  followers: 4
+
+};
